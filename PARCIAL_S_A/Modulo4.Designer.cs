b98ae2a15e87d6d6a28afc85f0551ac6f0e3b704
Braian Martinez <barmandomartinez@ucundinamarca.edu.co>
2021-09-22 16:43:23 -0500

    creacion diseño modulo 4

--- PARCIAL_S_A/Modulo4.Designer.cs
+++ PARCIAL_S_A/Modulo4.Designer.cs
@@ -29,12 +29,85 @@ namespace PARCIAL_S_A
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnInvertir = new System.Windows.Forms.Button();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::PARCIAL_S_A.Properties.Resources.modulo_4_img;
+            this.panel1.Controls.Add(this.btnInvertir);
+            this.panel1.Controls.Add(this.txtPalabra);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(797, 445);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnInvertir
+            // 
+            this.btnInvertir.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInvertir.Location = new System.Drawing.Point(313, 265);
+            this.btnInvertir.Name = "btnInvertir";
+            this.btnInvertir.Size = new System.Drawing.Size(183, 62);
+            this.btnInvertir.TabIndex = 3;
+            this.btnInvertir.Text = "Invertir";
+            this.btnInvertir.UseVisualStyleBackColor = true;
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(221, 199);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(386, 20);
+            this.txtPalabra.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label2.Location = new System.Drawing.Point(281, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Invertir Palabras";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(218, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa la palabra para invertirla";
+            // 
+            // Modulo4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Modulo4";
             this.Text = "Modulo4";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnInvertir;
+        private System.Windows.Forms.TextBox txtPalabra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
