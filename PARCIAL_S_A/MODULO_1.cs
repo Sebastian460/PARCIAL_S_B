65e2edd69b061e20cd35e88bff8eeaff39ebaaa5
Sebastian Sanchez <sebastiansanchez460.ss@outlook.com>
2021-09-22 11:32:03 -0500

    ESE ES EL PRIMER MODULO CON EL EJERCICIO INDICADO

--- PARCIAL_S_A/MODULO_1.cs
+++ PARCIAL_S_A/MODULO_1.cs
@@ -12,9 +12,41 @@ namespace PARCIAL_S_A
 {
     public partial class MODULO_1 : Form
     {
+        private object txtnumero1;
+        private object txtnumero2;
+
         public MODULO_1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+            int mayor;
+
+            numero1 = Convert.ToInt32(textBox1.Text);
+            numero2 = Convert.ToInt32(textBox2.Text);
+
+            if (numero1 >= numero2)
+                mayor = numero1;
+            else
+                mayor = numero2;
+
+            textBox3.Text = mayor.ToString();
+
+
         }
     }
 }
